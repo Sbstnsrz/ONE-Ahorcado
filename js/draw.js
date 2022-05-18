8c21aaff7f8b208d352561d2baef4768fce3d02c
Seb <sbstn1@gmail.com>
2022-05-17 23:54:58 -0300

    guiones agregados con hr

--- js/draw.js
+++ js/draw.js
@@ -10,55 +10,44 @@ draw.strokeStyle="#0A3871";
 //base
 draw.fillRect(0,355,294,360);
 
-function dibujar(contador){
-    switch(contador){
-        case 1:{
-            //mastil 1
-            draw.fillRect(80,0,5,360);
-            break;}
-        case 2:{
-            //mastil 2
-            draw.fillRect(85,0,174,5);
-            break;}
-        case 3:{
-            //mastil 3
-            draw.fillRect(254,5,5,50);
-            break;}
-        case 4:{
-            //cabeza
-            draw.arc(256, 80, 25, 0, 2 * Math.PI);
-            draw.stroke();
-            break;}
-        case 5:{
-            //cuerpo
-            draw.fillRect(254,105,5,135);
-            break;}
-        case 6:{
-            //brazo derecho
-            draw.moveTo(256,105);
-            draw.lineTo(256+32,105+62);
-            draw.stroke();
-            break;}
-        case 7:{
-            //brazo izquierdo
-            draw.moveTo(256,105);
-            draw.lineTo(256-32,105+62);
-            draw.stroke();
-            break;}
-        case 8:{
-            //Pierna derecha
-            draw.moveTo(256,135+105);
-            draw.lineTo(256+32,135+105+62);
-            draw.stroke();
-            break;}
-        case 9:{
-            //Pierna izquierda
-            draw.moveTo(256,135+105);
-            draw.lineTo(256-32,135+105+62);
-            draw.stroke();
-            break;
-        }
-        default:
-            return;
+function dibujar(cont){
+    switch(cont){
+        case 1:{//mastil 1
+                draw.fillRect(80,0,5,360);
+                break;}
+        case 2:{//mastil 2
+                draw.fillRect(85,0,174,5);
+                break;}
+        case 3:{//mastil 3
+                draw.fillRect(254,5,5,50);
+                break;}
+        case 4:{//cabeza
+                draw.arc(256, 80, 25, 0, 2 * Math.PI);
+                draw.stroke();
+                break;}
+        case 5:{//cuerpo
+                draw.fillRect(254,105,5,135);
+                break;}
+        case 6:{//brazo derecho
+                draw.moveTo(256,105);
+                draw.lineTo(256+32,105+62);
+                draw.stroke();
+                break;}
+        case 7:{//brazo izquierdo
+                draw.moveTo(256,105);
+                draw.lineTo(256-32,105+62);
+                draw.stroke();
+                break;}
+        case 8:{//Pierna derecha
+                draw.moveTo(256,135+105);
+                draw.lineTo(256+32,135+105+62);
+                draw.stroke();
+                break;}
+        case 9:{//Pierna izquierda
+                draw.moveTo(256,135+105);
+                draw.lineTo(256-32,135+105+62);
+                draw.stroke();
+                break;}
+        default:return;
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var letras = document.querySelector(".letrasErradas");
-letras.textContent="";
 
 var contador=0;
 window.addEventListener('keydown', function(event){
     var key = event.key.toUpperCase();
     if(event.key && contador<9 && letras.textContent.indexOf(key)==-1){
+        verificarPalabra(key);
         letras.textContent+=key;
         contador++;
         //funcion en draw.js
@@ -12,6 +12,37 @@ window.addEventListener('keydown', function(event){
         console.log(contador);
     }
     if(contador==9){
+        letras.classList.add("mensajePerdiste");
         letras.textContent="Lo siento, perdiste.";
     }
 });
+
+function verificarPalabra(key){
+    var letras = document.querySelectorAll("#contenedor-palabra");
+
+}
+
+function construirTd(dato, clase){
+    var td = document.createElement("td");
+    td.classList.add(clase);
+    td.textContent = dato;
+    return td;
+}
+function contruirGuion(){
+    var td = document.createElement("td");
+    var linea = document.createElement("hr");
+    td.appendChild(linea);
+
+    return td;
+}
+
+var contenedor = document.querySelector("#contenedor-Palabra");
+var guion = document.querySelector("#guion");
+
+var palabra="ALURA";
+//generar campos de letras
+for(i=0; i<palabra.length;i++){
+    var caracter = construirTd("","palabra");
+    contenedor.appendChild(caracter);
+    guion.appendChild(contruirGuion());
+}
